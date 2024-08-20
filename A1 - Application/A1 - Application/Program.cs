5a52857e86b0d7d24f98ef0f7f4bfbd816f8fda3
braidey-woodham <braidey.woodham01@student.weltec.ac.nz>
2024-08-20 15:00:43 +1200

    First Commit to Local Repo - VS Application

---
+++ A1 - Application/A1 - Application/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1___Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
